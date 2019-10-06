ce0d923569d25ad155aba7c7c50dac0522b039cf
iampk14 <54847707+iampk14@users.noreply.github.com>
2019-10-06 11:46:04 +0530

    Update Reciever_mpu6050.ino

--- Reciever_mpu6050/Reciever_mpu6050.ino
+++ Reciever_mpu6050/Reciever_mpu6050.ino
@@ -1,33 +1,50 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <Servo.h>   
+#include <Servo.h>
 
+//create an RF24 object
+RF24 radio(9, 8);  // CE, CSN
 Servo servoarm;
-Servo servobase;          
-
-int servo1 = 2;
-int servo2 =3;
-
-RF24 radio(9, 8); // CE, CSN
+Servo servobase;
+int servo1 = 5;
+int servo2 = 4;
+//address through which two modules communicate.
 const byte address[6] = "00001";
-
-void setup() {
-  Serial.begin(9600);
-  servoarm.attach (servo1);
-  servobase.attach (servo2);
+  //int ax,acc[2];
+void setup()
+{
+  while (!Serial);
+    Serial.begin(9600);
+  servoarm.attach(servo1); 
+  servobase.attach(servo2);
   radio.begin();
+  
+  
+  //set the address
   radio.openReadingPipe(0, address);
+  
+  //Set module as receiver
   radio.startListening();
 }
-void loop() {
-  if (radio.available()) {
-    int16_t ax = 0;
-    int16_t ay = 0;
-    radio.read(&ax, sizeof(ax));
-    radio.read(&ay, sizeof(ay));
-    servoarm.write (ax); 
-    servobase.write (ay);
-    Serial.println(ax); 
-  }
+
+void loop()
+{
+  //Read the data if available in buffer
+  if (radio.available())
+  {
+     int ax,ay;
+   radio.read(&ax,sizeof(ax));
+   radio.read(&ay,sizeof(ay));
+   
+   Serial.println(ax);  
+    Serial.println(ay);  
+   delay(2000);  
+  ax=map(ax,-17000,17000,0,180);
+  ay=map(ay,-17000,17000,0,180);
+  servoarm.write(ax);
+  servobase.write(ay);
+
+
+  }  
 }
