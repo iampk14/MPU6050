89e767d2d8c78ab8c2b08db731c09e145c8075b9
iampk14 <54847707+iampk14@users.noreply.github.com>
2019-10-11 21:09:34 +0530

    Update Transmitter_mpu6050.ino

--- Transmitter_mpu6050/Transmitter_mpu6050.ino
+++ Transmitter_mpu6050/Transmitter_mpu6050.ino
@@ -1,56 +1,54 @@
-#include <Wire.h>                // Including libraries
-#include <RF24.h>
-#include <nRF24L01.h>
+#include "Wire.h"
 #include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+
 #include <MPU6050.h>
+MPU6050 sensor;
 
-RF24 radio(9, 8);                // CE, CSN
+//create an RF24 object
+RF24 radio(9, 8);  // CE, CSN
 
-const byte address[6] = "00001"; //address through which two modules communicate.
+//address through which two modules communicate.
+const byte address[6] = "00001";
 
-MPU6050 sensor; // Initializing MPU6050
-int16_t AccX , AccY , AccZ, GyroX , GyroY , GyroZ; 
+int16_t ax ,ay, az, gx, gy, gz;
 const int ProxSensor=A3;
-int value_arrayX[6]={0},value_arrayY[6]={0},value,value_avgX=0,value_avgY=0,i,j;
+int value;
+
 
 void setup()
 {
   pinMode(ProxSensor,INPUT);
   Wire.begin();
-  Serial.begin(115200);
+  Serial.begin(9600);
   radio.begin();
   sensor.initialize();
+  //set the address
   radio.openWritingPipe(address);
-  radio.stopListening();  
+  
+  //Set module as transmitter
+  radio.stopListening();
 }
-
 void loop()
-{ 
- sensor.getMotion6(&AccX, &AccY, &AccZ, &GyroX, &GyroY, &GyroZ); //Taking values of Accelerometer
- value = analogRead(ProxSensor);                                 //Taking values of Ir 
- if(value>=100)
- {
-  value = 1;
- }
- else
- {
-  value = 0;
- }
- value_arrayX[0]=AccX;
- value_arrayY[0]=AccY;
- value_avgX=0;
- i=0;
- for(i=0;i<6;i++)
-    value_avgX=(value_avgX+value_arrayX[i])/6;                   //Taking Average
-  value_avgY=0;
-  j=0;
-  for(j=0;j<6;j++)
-    value_avgY=(value_avgY+value_arrayY[j])/6;                   //Taking Average
- radio.write(&value, sizeof(value));                             //Transmitting Data          
- radio.write(&value_avgX, sizeof(value_avgX));
- radio.write(&value_avgY, sizeof(value_avgY)); 
- Serial.println(value);
- Serial.println(value_avgX);
- Serial.println(value_avgY);
- delay(25);
- }
+{
+  
+  sensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  radio.write(&ax, sizeof(ax));
+  radio.write(&ay, sizeof(ay));
+  Serial.println(ax);
+  Serial.println(ay);
+  value = analogRead(ProxSensor);
+  if(value>=100)
+  {
+    value=1;
+  }
+  else
+  {
+    value=0;
+  }
+  radio.write(&value, sizeof(value));
+  Serial.println(value);
+ delay(4);
+} 
